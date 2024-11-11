9fe349808214c4573009898df6712dee71297bd9
Marcos José <marcosjoseDV@hotmail.com>
2024-11-11 17:28:34 -0300

    Resumo, Teste um completo.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Estagio.Tecnica;
+
+Teste01.ValorDaSoma();
+Teste02
---
+++ Tecnica/Teste01.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estagio.Tecnica;
+
+public static class Teste01
+{
+    public static void ValorDaSoma()
+    {
+        int indice = 13;
+        int soma = 0;
+        int k = 0;
+
+        while (k < indice)
+        {
+            k++;
+            soma += k;
+        }
+        Console.WriteLine("---------------");
+        Console.WriteLine("Teste 01 - Soma");
+        Console.WriteLine(soma);
+        Console.WriteLine("---------------\n\n");
+    }
+}
